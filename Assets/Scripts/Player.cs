ef03ae8fb21e6faec8a8297cd5a77b42dd3852bb
THOMAS KOHUT <thomaskohut@mail.adelphi.edu>
2021-07-01 16:14:46 -0400

    Finished version (as of upload) with notes and cleanup

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,8 @@ public class Player : MonoBehaviour
 {
     //Determines speed
     [SerializeField]
-    private float speed = 5f;
+    private float _speed = 5.0f;
+   
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +19,24 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
 
-        //transform.Translate(Vector3.up * speed * Time.deltaTime);
-        //transform.Translate(Vector3.down * speed * Time.deltaTime);
-        //transform.Translate(Vector3.left * (speed * 1.5f) * Time.deltaTime);
+        //1 line version of below 2 lines
+        //transform.Translate(new Vector3(hInput, vInput, 0)* _speed * Time.deltaTime);
 
-        transform.Translate(Vector3.right * speed/2 * Time.deltaTime);
+        transform.Translate(Vector3.right * hInput * _speed * Time.deltaTime);
+        transform.Translate(Vector3.up * vInput * _speed * Time.deltaTime);
+
+        //if playerpos on x is greater than 9 or less than -9, then it is set to its edge
+        if(transform.position.x < -11.3f || transform.position.x > 11.3f)
+        {
+            transform.position = new Vector3(-1*transform.position.x, transform.position.y, transform.position.z);
+        }
+        if (transform.position.y < -7.56f || transform.position.y > 7.56f)
+        {
+            transform.position = new Vector3(transform.position.x, -1 * transform.position.y, transform.position.z);
+        }
+        //To prevent objects from leaving screen (no wrap), use Mathf.Clamp()
     }
-}
+    }
