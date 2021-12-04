f5c15db1a0f10ba271ed8f95624fbc3e6670745a
THOMAS KOHUT <thomaskohut@mail.adelphi.edu>
2021-12-04 16:00:14 -0500

    Added spawn manager and assets

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _eContainer;
+    [SerializeField]
+    private GameObject _enemyprefab;
+    [SerializeField]
+    private GameObject _playerprefab;
+
+    private bool _spActive = false;
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnDeath()
+    {
+        _spActive = true;
+    }
+
+    //Spawns enemy every 5 seconds
+    IEnumerator SpawnEnemy()
+    {
+        while (!_spActive)
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-9f, 9f), 7.56f, 0);
+            GameObject newEnemy = Instantiate(_enemyprefab, spawnPos, Quaternion.identity);
+            newEnemy.transform.parent = _eContainer.transform;
+            yield return new WaitForSeconds(5.0f);
+        }
+    }
+}
+
+
